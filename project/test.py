095351f9aceb3bc8fd5a9aeed2a1dea0c7c5b196
Piotr C <cwiecekpiotr@gmail.com>
2015-01-26 19:06:20 +0000

    Dzialajacy 1

--- project/test.py
+++ project/test.py
@@ -32,10 +32,10 @@ def getContentDataFromUrl(url):
     soup = BeautifulSoup(content)
     soup2 = BeautifulSoup(content)
     for soup in soup.find_all(text=re.compile("^(art.) ([0-9]{1,4})")):
-        dataArt.append(soup)
+        dataArt.append(soup.encode('utf-8'))
 
     for date in soup2.find_all(text=re.compile("^([0-9]{1,2} [a-z]* [0-9]{4})")):
-        dataDate.append(date)
+        dataDate.append(date.encode('utf-8'))
 
     # for price in soup.find_all(text=re.compile("^(?:\d*\.[0-9]{3} zł)|(?:\d*\.[0-9]{3},[0-9]{2} zł)")):
     #     dataPrice.append(price)
@@ -52,10 +52,6 @@ def getMetricDataFromLink(url):
         dtarr.append(dtTmp.get_text().encode('utf-8'))
     resultMetric = dict(zip(dtarr, ddarr))
     return resultMetric
-
-    # for key, val in resultMetric.items():
-    #     if key == "Przewodniczący:":
-    #         print val
 
 def getAllLinksFromRss(rss):
     feed = feedparser.parse(rss)
@@ -75,99 +71,102 @@ rss = 'http://orzeczenia.piotrkow-tryb.so.gov.pl/rsscontent/15200000'
 getAllLinksFromRss(rss);
 checkExistLinkInDatabase()
 url = 'http://orzeczenia.krakow.sa.gov.pl/details/Odszkodowanie/152000000000503_I_ACa_000592_2013_Uz_2014-04-17_001'
-getContentDataFromUrl(url)
+
 # pprint.pprint(dataDate)
 # pprint.pprint(dataArt)
 # pprint.pprint(linksNew)
 # print linksNew
 # i = 0
+if len(linksNew):
+    for linkurl in linksNew:
 
-for linkurl in linksNew:
+        url = linkurl
+        rssId = 0
 
-    url = linkurl
-    rssId = 0
+        getContentDataFromUrl(url)
 
-    getContentDataFromUrl(url)
+        artDup = list(set(dataArt))
+        dateDup = list(set(dataDate))
 
-    artDup = list(set(dataArt))
-    dateDup = list(set(dataDate))
+        # for a in artDup:
+        #     print a
+        rssId =  Rss.select().where(Rss.link == rss).get()
 
-    rssId =  Rss.select().where(Rss.link == rss).get()
+        metric = getMetricDataFromLink(url)
 
-    metric = getMetricDataFromLink(url)
+        title = ''
+        date_of_judgment = ''
+        date_publication = ''
+        signature = ''
+        judgment_name = ''
+        judge = ''
+        faculty = ''
+        chairman = ''
+        recorder = ''
+        legal_basis = ''
 
-    title = ''
-    date_of_judgment = ''
-    date_publication = ''
-    signature = ''
-    judgment_name = ''
-    judge = ''
-    faculty = ''
-    chairman = ''
-    recorder = ''
-    legal_basis = ''
+        for key, val in metric.items():
+            if key == "Tytuł:":
+                title = val
+            elif key == "Data orzeczenia:":
+                date_of_judgment = val
+            elif key == "Data publikacji:":
+                date_publication = val
+            elif key == "Sygnatura:":
+                signature = val
+            elif key == "Sędziowie:":
+                judge = val
+            elif key == "Sąd:":
+                judgement_name = val
+            elif key == "Protokolant:":
+                recorder = val
+            elif key == "Podstawa prawna:":
+                legal_basis = val
+            elif key == "Przewodniczący:":
+                chairman = val
+            elif key == "Wydział:":
+                faculty = val
 
-    for key, val in metric.items():
-        if key == "Tytuł:":
-            title = val
-        elif key == "Data orzeczenia:":
-            date_of_judgment = val
-        elif key == "Data publikacji:":
-            date_publication = val
-        elif key == "Sygnatura:":
-            signature = val
-        elif key == "Sędziowie:":
-            judge = val
-        elif key == "Sąd:":
-            judgement_name = val
-        elif key == "Protokolant:":
-            recorder = val
-        elif key == "Podstawa prawna:":
-            legal_basis = val
-        elif key == "Przewodniczący:":
-            chairman = val
-        elif key == "Wydział:":
-            faculty = val
+        try:
 
-    try:
-
-        metadata = MetaData.create(
-            links = url,
-            rss = rssId
-        )
-
-        judgmentResult = Judgment(
-            title = title,
-            date_of_judgment = date_of_judgment,
-            date_publication = date_publication,
-            signature = signature,
-            judgement_name = judgement_name,
-            judges = judge,
-            faculty = faculty,
-            chairman = chairman,
-            recorder = recorder,
-            legal_basis = legal_basis,
-            metadata = metadata,
-            cities = 3
-        )
-
-        judgmentResult.save(force_insert=True)
-        for tmpArt in artDup:
-            keys = Key.create(
-                typ = 'art',
-                value = tmpArt,
-                judgment = judgmentResult,
+            metadata = MetaData.create(
+                links = url,
+                rss = rssId
             )
-        for tmpDate in dateDup:
-            keys = Key.create(
-                typ = 'date',
-                value = tmpDate,
-                judgment = judgmentResult,
-            )
-        print 'Wykonało się'
-    except:
-        raise
 
+            judgmentResult = Judgment.create(
+                title = title,
+                date_of_judgment = date_of_judgment,
+                date_publication = date_publication,
+                signature = signature,
+                judgement_name = judgement_name,
+                judges = judge,
+                faculty = faculty,
+                chairman = chairman,
+                recorder = recorder,
+                legal_basis = legal_basis,
+                metadata = metadata,
+                cities = 3
+            )
+
+
+            for tmpArt in artDup:
+                keys = Key.create(
+                    typ = 'art',
+                    value = tmpArt,
+                    judgment = judgmentResult,
+                )
+            for tmpDate in dateDup:
+                keys = Key.create(
+                    typ = 'date',
+                    value = tmpDate,
+                    judgment = judgmentResult,
+                )
+            print 'Wykonało się'
+        except:
+            raise
+else:
+    print 'Brak linków lub wszystkie linki zostały dodane'
 
 # print metric
 # getAllLinksFromRss(url)
